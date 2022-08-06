5301eeb48c692edc7af07627393c4dc074ceca07
Giliardson Cezar <98276270+giliardson@users.noreply.github.com>
2022-08-06 20:20:47 -0300

    finalizado

--- js/exercicio/ex03/script.js
+++ js/exercicio/ex03/script.js
@@ -11,15 +11,33 @@ function verificar(){
         var foto = ''
         if(genero[0].checked){
             generoMarcado = 'Maculina'
-            if (idade >=0 && idade <=10 ){
+            if (idade >=0 && idade <10 ){
                 //crianca
                 foto = 'imagem/homem-bebe.png'
+            } else if (idade < 20){
+                //jovem
+                foto = 'imagem/homem-jovem.png'
+            } else if (idade < 55){
+                //adulto
+                foto = 'imagem/homem-adulto.png'
+            } else{
+                //velho
+                foto = 'imagem/homem-velho.png'
             }
         } else{
             generoMarcado = 'Feminina'
-            if (idade >=0 && idade <=10 ){
+            if (idade >=0 && idade <10 ){
                 //crianca
                 foto = 'imagem/mulher-bebe.png'
+            } else if (idade < 20){
+                //jovem
+                foto = 'imagem/mulher-jovem.png'
+            } else if (idade < 55){
+                //adulto
+                foto = 'imagem/mulher-adulta.png'
+            } else{
+                //velho
+                foto = 'imagem/mulher-velho.png'
             }
         }
 
